f6b5cf3335a9b5c95f556f0fba3ec6765491b91f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-06-14 22:04:36 +0300

    Fix bucket name for remote state

--- 00-variables.tf
+++ 00-variables.tf
@@ -18,7 +18,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config {
-    bucket = "terraform-state-${var.stage}"
+    bucket = "hollowverse-terraform-state-${var.stage}"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
