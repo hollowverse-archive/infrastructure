978906bc019367234bf7f4aa02a3bb3d94d40116
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-06-14 21:47:15 +0300

    Configure S3 state backend

--- 00-variables.tf
+++ 00-variables.tf
@@ -3,18 +3,35 @@ provider "aws" {
 }
 
 variable "stage" {
-  type = "string"
+  type    = "string"
   default = "development"
+}
+
+terraform {
+  backend "s3" {
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-state-${var.stage}"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 locals {
   common_tags = {
     Terraform = "True"
-    Stage = "${var.stage}"
+    Stage     = "${var.stage}"
   }
 }
 
 locals {
-  log_queue_name = "logging-queue-${var.stage}"
+  log_queue_name             = "logging-queue-${var.stage}"
   dead_letter_log_queue_name = "logging-dead-letter-queue-${var.stage}"
 }
