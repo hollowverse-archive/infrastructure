ee3842ae05d0ef6427c022e5440d986210c5ab7b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-06-15 21:33:56 +0300

    Add additional VPC AZ

--- 01-vpc.tf
+++ 01-vpc.tf
@@ -2,17 +2,17 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "1.32.0"
 
-  name               = "vpc-${var.stage}"
+  name = "vpc-${var.stage}"
 
-  cidr = "10.0.0.0/16"
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
-  azs = ["us-east-1a", "us-east-1b"]
+  cidr            = "10.0.0.0/16"
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  enable_dns_support = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = "${local.common_tags}"
