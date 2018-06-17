6c6e5050139c5345707162696d97755d68bc1243
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-06-17 11:40:35 +0300

    Define photos infrastructure

---
+++ 04-photos.tf
@@ -0,0 +1,70 @@
+# This file defines all the infrastructure needed for storing and processing
+# images on hollowverse.com
+
+# An S3 bucket to store unprocessed images (e.g. images of notable people fetched
+# from Wikipedia)
+resource aws_s3_bucket "unprocessed_photos_bucket" {
+  bucket = "hollowverse-photos-unprocessed-${var.stage}"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = "${local.common_tags}"
+}
+
+# An S3 bucket to store the processed, production-ready photos
+resource aws_s3_bucket "processed_photos_bucket" {
+  bucket = "hollowverse-photos-processed-${var.stage}"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = "${local.common_tags}"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity_for_photos_bucket" {
+  comment = "Some comment"
+}
+
+resource aws_cloudfront_distribution "photos_cloudfront_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.processed_photos_bucket.bucket_regional_domain_name}"
+    origin_id   = "processed-photos-bucket"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity_for_photos_bucket.cloudfront_access_identity_path}"
+    }
+  }
+
+  aliases = "${compact(list(
+    "photos-${var.stage}.hollowverse.com",
+    "${var.stage == "production" ? "photos-1.hollowverse.com" : ""}"
+  ))}"
+
+  # Reference: https://aws.amazon.com/cloudfront/pricing/
+  price_class = "PriceClass_100"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    fowarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  logging {
+    bucket = "${aws_s3_bucket.logging_bucket.bucket_domain_name}"
+    prefix = "photos/"
+  }
+
+  tags = "${local.common_tags}"
+}
